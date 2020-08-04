211ed629562bad9f65e6b0c76518a95c1a0af1a6
faranak Sabet <faranaksabet@yahoo.com>
2020-08-04 10:01:26 -0700

    added time

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-var date = new Date(moment().format("LLLL"));
-var present = moment().format("HH");
-var present = moment().format("HH");
+var time = moment().format("LTS");
+var date = moment().format("l");
+
+var displayTime = document.getElementById("time");
+var displayDate = document.getElementById("date");
+
+setInterval(() => {
+  displayTime.innerHTML = moment().format("LTS");
+  displayDate.innerHTML = moment().format("l");
+  checkTime();
+}, 500);
+function checkTime() {
+  for (let i = 9; i < 17; i++) {
+    if (i > moment().format("h")) {
+      document.getElementById(`${i}`).style.backgroundColor = "red";
+    }
+  }
+}
+console.log(moment().format("h"));
